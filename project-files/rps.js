d1437af9ebd20d953f78ac53fdaa0bd38d1cd074
Gavin Rockwell <gsrockwell910@gmail.com>
2022-10-13 11:30:37 -0400

    Add a score system, a five round loop, and a winner/loser message

--- project-files/rps.js
+++ project-files/rps.js
@@ -1,3 +1,7 @@
+//Start variables for score 
+let playerScore = 0;
+let computerScore = 0;
+//End variables for score
 //Start function to determine computers play
 const getComputerChoice = () => {
     const options = ["rock", "paper", "scissors"];
@@ -15,22 +19,41 @@ const playRound = (playerSelection, computerSelection) => {
     } else if (playerSelection === "scissors" && computerSelection === "scissors") {
         return "You tied! you both threw scissors!"
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++
         return "You won! rock crushes scissors!"
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++
         return "You won! paper covers rock!"
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++
         return "You won! scissors cuts paper!"
     } else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore++
         return "You lost! paper covers rock!"
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore++
         return "You lost! scissors cuts paper!"
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore++
         return "You lost! rock crushes scissors!"
     }
 }
 
 const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-
-console.log(playRound(playerSelection, computerSelection));
 //End function to play a single round
+//Start function to loop a five round game 
+const game = () => {
+    for (let i = 0; i < 5; i++) {
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    }
+    if (playerScore > computerScore) {
+        return "You won! Absolutely remarkable";
+    } else if (playerScore < computerScore) {
+        return "You lost! Now the computers shall rule us all!"
+    } else {
+        return "You tied! What a fateful ending"
+    }
+}
+console.log(game());
+//End function to loop a five round game 
